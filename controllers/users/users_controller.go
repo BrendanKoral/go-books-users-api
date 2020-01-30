6a86f7e4217e516703004c93b4f59042f7ff646c
Brendan Koral <hipposmelas@gmail.com>
2020-01-30 13:50:30 -0600

    Add 400 bad request on CreateUser

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,6 +3,7 @@ package users
 import (
 	"github.com/brendankoral/go-books-users-api/domain/users"
 	"github.com/brendankoral/go-books-users-api/services"
+	"github.com/brendankoral/go-books-users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,8 +12,12 @@ func CreateUser(c *gin.Context) {
 	var user users.User
 
 	if err := c.ShouldBindJSON(&user); err != nil {
-		//TODO: handle JSON error
-		//TODO: return bad request to caller
+		restErr := errors.RestErr{
+			Message:"invalid JSON body",
+			Status: http.StatusBadRequest,
+			Error: "bad_request",
+		}
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 
